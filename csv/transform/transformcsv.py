91bbe1fc9853aa392a9695da35594faec519439b
Jaap Koetsier <jaap@chariot-it.nl>
2018-06-01 12:11:07 +0200

    Multithreaded transformcsv.py, plus timing statistics

--- csv/transform/transformcsv.py
+++ csv/transform/transformcsv.py
@@ -1,8 +1,16 @@
 import sys
 import os
 import re
+import threading
+import time
+import itertools
 
-linebatchsize = 500
+outputbuffersize_lines = 1000
+inputchunck = 1000
+maxthreads = 8
+globaltotallines = 0
+globalrunningtime = 0.0
+lock = threading.Condition()
 
 '''
 Changes occurrences of the format "20/12/2016 20:08:51 +00:00" or "20/12/2016 20:08:51" to
@@ -14,6 +22,9 @@ def transformdates(line: str):
     return re.sub(r'"(?P<day>\d+)/(?P<month>\d+)/(?P<year>\d+) (?P<time>\d+:\d+:\d+)( \+\d+:\d+)?"',
                   "\"\g<year>-\g<month>-\g<day> \g<time>\"", line)
 
+def transformbogusdates(line: str):
+    return re.sub(r'"1899-(?P<rest>\d+-\d+ \d+:\d+:\d+)"', '"1970-\g<rest>"', line)
+
 
 def transformemptystrings(line: str) -> str:
     return re.sub(r',(""(?=,)|""$)', ",NULL", line)
@@ -23,40 +34,83 @@ def transformbooleans(line: str) -> str:
     line = re.sub(r'"true"', "1", line, flags=re.I)
     return re.sub(r'"false"', "0", line, flags=re.I)
 
-
 def transformline(line: str) -> str:
     result = line
 
     result = transformdates(result)
+    result = transformbogusdates(result)
     result = transformemptystrings(result)
     result = transformbooleans(result)
 
     return result
 
 
-def transformcsv(file: str, outputdir: str):
+def transformcsv(file: str, outputdir: str) -> int:
+    global globaltotallines
+    global globalrunningtime
+
+    start = time.time()
     filename = file.split("/")[-1]
     dstfile = outputdir + "/" + filename
 
     if os.path.exists(dstfile):
         os.remove(dstfile)
 
+    ## Per line
+    # with open(file, "r") as inputfile:
+    #     with open(dstfile, "a") as outputfile:
+    #         outputbuffer = []
+    #         for inputline in inputfile:
+    #             outputbuffer.append(transformline(inputline))
+    #
+    #             if len(outputbuffer) > outputbuffersize_lines:
+    #                 outputfile.writelines(outputbuffer)
+    #                 outputbuffer = []
+    #
+    #         outputfile.writelines(outputbuffer)
+
+    ## Test with islice
+
+    totallines = 0
     with open(file, "r") as inputfile:
         with open(dstfile, "a") as outputfile:
-            inputlines = inputfile.readlines()
-
             outputbuffer = []
-            for inputline in inputlines:
-                outputbuffer.append(transformline(inputline))
 
-                if len(outputbuffer) > linebatchsize:
-                    outputfile.writelines(outputbuffer)
-                    outputbuffer = []
+            while True:
+                inputlines = list(itertools.islice(inputfile, inputchunck))
+                totallines += len(inputlines)
+
+                if not inputlines:
+                    break
+
+
+                for inputline in inputlines:
+                    outputbuffer.append(transformline(inputline))
+
+                    if len(outputbuffer) > outputbuffersize_lines:
+                        outputfile.writelines(outputbuffer)
+                        outputbuffer = []
 
             outputfile.writelines(outputbuffer)
 
+    end = time.time()
+
+    passed = end - start
+    print("Done %s (%d lines) in %f seconds. %f lines/sec" % (file, totallines, passed, (float(totallines) / passed)))
+
+    lock.acquire()
+    globaltotallines += totallines
+    globalrunningtime += passed
+    lock.release()
+
+    return totallines
+
 
 def transformcsvfiles(directory: str):
+    global globaltotallines
+    global globalrunningtime
+
+    start = time.time()
     if directory[-1] == "/":
         directory = directory[0:-1]
 
@@ -67,9 +121,38 @@ def transformcsvfiles(directory: str):
 
     files = [ f for f in os.listdir(directory) if os.path.isfile(directory + "/" + f) and f.endswith(".csv") ]
 
-    for i, filename in enumerate(files):
-        transformcsv(directory + "/" + filename, outputdir)
-        print("{:3}/{} {} done".format(i + 1, len(files), filename))
+    files_w_length = []
+
+    for f in files:
+        fpath = directory + "/" + f
+        st_size = os.stat(fpath).st_size
+
+        files_w_length.append({
+            "filepath": fpath,
+            "size": st_size
+        })
+
+    sorted_files = sorted(files_w_length, key=lambda k: k['size'])
+
+
+    while True:
+
+        while threading.active_count() < maxthreads and len(sorted_files) > 0:
+            t = threading.Thread(target=transformcsv, args=[sorted_files.pop()['filepath'], outputdir])
+            t.start()
+
+        if threading.active_count() == 1 and len(sorted_files) == 0:
+            break
+
+    end = time.time()
+    passed = end - start
+
+    print("We're done. Took %f seconds in total for %d lines. Avg %f lines/sec. Total thread running time: "
+          "%s sec. Avg: %f lines/sec" %
+          (passed, globaltotallines, float(globaltotallines) / passed, globalrunningtime,
+           float(globaltotallines) / globalrunningtime))
+
+
 
 
 if __name__ == "__main__":
